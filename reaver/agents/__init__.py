afbe9e7578e0f6bf46f53245d0a1ac2267738a8e
Jake Sage <jsagehome@gmail.com>
2018-12-11 16:55:03 -0500

    again

--- reaver/agents/__init__.py
+++ reaver/agents/__init__.py
@@ -2,6 +2,7 @@ from .base import *
 from .random import RandomAgent
 from .a2c import AdvantageActorCriticAgent
 from .ppo import ProximalPolicyOptimizationAgent
+from .testagent import RandomActorCriticAgent
 
 A2C = AdvantageActorCriticAgent
 PPO = ProximalPolicyOptimizationAgent
